c8fb6036469bf7243236b03e96ce6352c3d71d4a
Pau Estalella <pau.estalella@gmail.com>
2020-01-26 00:29:15 +0100

    Improve timing of execution unit
    Sync the ALU to the clock, to avoid very long signal paths.
    Request register reads in the DECODE stage to have the data ready in the
    EXECUTE stage.
    Add proper carry and zero flag bits, that keep their value across instructions
    and are only modified by arithmetic instructions.

--- src/design/alu.sv
+++ src/design/alu.sv
@@ -4,23 +4,25 @@
 `timescale 1ns / 1ps
 
 module alu  #(parameter DATA_BITS  = 8) (
+    input wire clk,
     input wire reset,
     input wire [DATA_BITS-1:0] a,
     input wire [DATA_BITS-1:0] b,
     input wire cin,
     output logic [DATA_BITS-1:0] result,
-    output wire cout,
-    output wire zero
+    output logic cout,
+    output logic zero
     );
 
-    bit zero_reg;
+    logic [DATA_BITS:0] reg_result;
 
-    assign zero = zero_reg;
     // If cin==1, it means the request is to subtract instead of add
-    assign {cout, result} = cin? (a + ~b + cin) : (a + b);
+    assign reg_result = cin? (a + ~b + cin) : (a + b);
 
-    always_comb
-        zero_reg <= reset? 0 : ~(|{cout, result});
+    always_ff @(posedge clk) begin
+        {cout, result} <= reg_result;
+                  zero <= reset? 0 : ~(|reg_result);
+    end
 endmodule
 
 `endif
--- src/design/execution_unit.sv
+++ src/design/execution_unit.sv
@@ -12,9 +12,6 @@
 import constants_pkg::*;
 import isa_pkg::*;
 
-typedef enum bit [2:0] {IDLE, FETCH_MSB_IR, FETCH_LSB_IR, DECODE,
-                        EXECUTE, LOAD_STAGE, STORE_START, STORE_END} ExecutionStage;
-
 module exec_unit #(parameter DATA_BITS = 8) (
     input wire clk,
     input wire reset,
@@ -30,6 +27,8 @@ module exec_unit #(parameter DATA_BITS = 8) (
 
     bit [constants_pkg::INSTRUCTION_POINTER_BITS-1:0] pc;
     bit [15:0] ir;
+    bit carry_flag;
+    bit zero_flag;
 
     logic rd_mem_en;
     logic [MEMORY_DATA_BITS-1:0] rd_mem_data;
@@ -57,6 +56,7 @@ module exec_unit #(parameter DATA_BITS = 8) (
     bit [REGISTER_ADDRESS_BITS-1:0] reg_rd0_addr, reg_rd1_addr, reg_wr_addr;
     bit reg_rd0_en, reg_rd1_en, reg_wr_en;
     enum bit {REGISTER_FILE, IMMEDIATE} alu_inputB_sel;
+    bit save_alu_flags;
 
     enum bit[1:0] {ALU_OUTPUT = 0,
                    INST_IMMEDIATE = 1,
@@ -64,7 +64,8 @@ module exec_unit #(parameter DATA_BITS = 8) (
                    REG_FILE_RD0 = 3} reg_input_sel;
 
     alu #(.DATA_BITS(REGISTER_DATA_BITS))
-        arith_unit(.reset(reset),
+        arith_unit(.clk(clk),
+                   .reset(reset),
                    .a(regfile_rd0_data),
                    .b(alu_input_b),
                    .cin(subtract),
@@ -101,16 +102,16 @@ module exec_unit #(parameter DATA_BITS = 8) (
                               .in3(regfile_rd0_data),
                               .out(register_file_input));
 
-    enum bit [1:0] {RESET = 0, NEXT_INSTRUCTION = 1, JUMP_TARGET = 2, UNDEFINED = 3} pc_offset_sel;
+    enum bit [1:0] {RESET = 0, NEXT_INSTRUCTION = 1, JUMP_TARGET = 2, NO_UPDATE = 3} pc_offset_sel;
     wire [JUMP_OFFSET_BITS-1:0] next_pc_input;
     bit [JUMP_OFFSET_BITS-1:0] jump_dest;
 
     mux4to1 #(.DATA_BITS(JUMP_OFFSET_BITS))
         pc_offset_mux(.sel(pc_offset_sel),
                       .in0('0),
-                      .in1((state==FETCH_LSB_IR)? 8'(pc + 2) : pc),
+                      .in1(8'(pc + 2)),
                       .in2(jump_dest),
-                      .in3('z),
+                      .in3(pc),
                       .out(next_pc_input));
 
     // Mostly to show in waves what the current instruction is
@@ -120,6 +121,63 @@ module exec_unit #(parameter DATA_BITS = 8) (
         if (state != IDLE)
             pc <= next_pc_input;
     end
+
+    function void request_register_reads;
+        case (ir[15:12])
+            MOVIR: begin
+            end
+            LOAD: begin
+            end
+            STORE: begin
+                reg_rd0_addr <= ir[10:8];
+                reg_rd0_en   <= 1;
+            end
+            ADDRR: begin
+                reg_rd0_addr <= rd_ram_data[6:4];
+                reg_rd1_addr <= rd_ram_data[2:0];
+                reg_rd0_en     <= 1;
+                reg_rd1_en     <= 1;
+                // Enable input to the ALU from the register file
+                alu_inputB_sel <= REGISTER_FILE;
+                // Addition op, therefore subtract=0
+                subtract       <= 0;
+            end
+             ADDI: begin
+                reg_rd0_addr   <= ir[10:8];
+                reg_rd0_en     <= 1;
+                inst_immediate <= rd_ram_data[7:0];
+                // Enable input to the ALU from the constant in the instruction
+                alu_inputB_sel <= IMMEDIATE;
+                // Addition op, therefore subtract=0
+                subtract       <= 0;
+            end
+            SUBRR: begin
+                reg_rd0_addr  <= rd_ram_data[6:4];
+                reg_rd1_addr  <= rd_ram_data[2:0];
+                reg_rd0_en    <= 1;
+                reg_rd1_en    <= 1;
+                // Enable input to the ALU from the register file
+                alu_inputB_sel <= REGISTER_FILE;
+                // Subtract op, therefore subtract=1
+                subtract <= 1;
+            end
+             SUBI: begin
+                reg_rd0_addr   <= ir[10:8];
+                reg_rd0_en     <= 1;
+                inst_immediate <= rd_ram_data[7:0];
+                alu_inputB_sel <= IMMEDIATE;
+                // Subtract op, therefore subtract=1
+                subtract <= 1;
+            end
+             JNZI: begin
+            end
+              JZR: begin
+            end
+              NOP: begin
+            end
+          default: $display("Invalid opcode %b", ir[15:12]);
+        endcase
+    endfunction
 
     function void execute_instruction;
         reg_input_sel <= ALU_OUTPUT;
@@ -147,20 +205,15 @@ module exec_unit #(parameter DATA_BITS = 8) (
             end
             STORE: begin
                 $display("store @%h r%0d", ir[7:0], ir[10:8]);
-                // Launch register read
-                wr_mem_addr  <= ir[7:0];
-                reg_rd0_addr <= ir[10:8];
-                reg_rd0_en   <= 1;
+                // Launch memory write
+                wr_mem_addr           <= ir[7:0];
+                wr_mem_data           <= regfile_rd0_data;
+                mem_write_in_progress <= 1;
+                wr_mem_en             <= 1;
+                rd_mem_en             <= 0;
             end
             ADDRR: begin
                 $display("add r%0d r%0d r%0d", ir[10:8], ir[6:4], ir[2:0]);
-                // Enable input to the ALU from the register file
-                alu_inputB_sel <= REGISTER_FILE;
-                // Two registers reads
-                reg_rd0_addr <= ir[6:4];
-                reg_rd1_addr <= ir[2:0];
-                reg_rd0_en     <= 1;
-                reg_rd1_en     <= 1;
                 // Enable writes to the register file from the ALU
                 reg_input_sel  <= ALU_OUTPUT;
                 reg_wr_addr  <= ir[10:8];
@@ -170,39 +223,20 @@ module exec_unit #(parameter DATA_BITS = 8) (
             end
              ADDI: begin
                 $display("add r%0d #%h", ir[10:8], ir[7:0]);
-                inst_immediate <= ir[7:0];
-                alu_inputB_sel <= IMMEDIATE;
-                reg_rd0_addr   <= ir[10:8];
-                reg_rd0_en     <= 1;
                 // Enable writes to the register file from the ALU
                 reg_input_sel  <= ALU_OUTPUT;
                 reg_wr_addr    <= ir[10:8];
                 reg_wr_en      <= 1;
-                // Addition op, therefore subtract=0
-                subtract       <= 0;
             end
             SUBRR: begin
                 $display("sub r%0d r%0d r%0d", ir[10:8], ir[6:4], ir[2:0]);
-                // Enable input to the ALU from the register file
-                alu_inputB_sel <= REGISTER_FILE;
-                // Two registers reads
-                reg_rd0_addr  <= ir[6:4];
-                reg_rd1_addr  <= ir[2:0];
-                reg_rd0_en    <= 1;
-                reg_rd1_en    <= 1;
                 // Enable writes to the register file from the ALU
                 reg_input_sel <= ALU_OUTPUT;
                 reg_wr_addr   <= ir[10:8];
                 reg_wr_en     <= 1;
-                // Subtract op, therefore subtract=1
-                subtract <= 1;
             end
              SUBI: begin
                 $display("sub r%0d #%h", ir[10:8], ir[7:0]);
-                inst_immediate <= ir[7:0];
-                alu_inputB_sel <= IMMEDIATE;
-                reg_rd0_addr   <= ir[10:8];
-                reg_rd0_en     <= 1;
                 // Enable writes to the register file from the ALU
                 reg_input_sel  <= ALU_OUTPUT;
                 reg_wr_addr    <= ir[10:8];
@@ -237,6 +271,9 @@ module exec_unit #(parameter DATA_BITS = 8) (
             reg_rd1_addr      <= 0;
             reg_wr_addr       <= 0;
             subtract          <= 0;
+            carry_flag        <= 0;
+            zero_flag         <= 0;
+            save_alu_flags    <= 0;
             reg_input_sel     <= ALU_OUTPUT;
             alu_inputB_sel    <= REGISTER_FILE;
             reg_wr_en         <= 0;
@@ -245,16 +282,18 @@ module exec_unit #(parameter DATA_BITS = 8) (
             reg_wr_en         <= 0;
             state             <= IDLE;
             current_inst      <= NOP;
+            jump_dest         <= 0;
         end else begin
             case (state)
                 FETCH_MSB_IR: begin
+                    // save ALU flags from previous instruction if necessary
+                    zero_flag   <= save_alu_flags ? alu_zero : zero_flag;
+                    carry_flag  <= save_alu_flags ? alu_carry : carry_flag;
                     // Prepare read transaction
                     rd_mem_addr <= pc;
                     rd_mem_en   <= 1;
                     wr_mem_en   <= 0;
                     reg_wr_en   <= 0;
-                    // By default, pc = pc + 2
-                    pc_offset_sel <= NEXT_INSTRUCTION;
                     state       <= FETCH_LSB_IR;
                 end
                 FETCH_LSB_IR: begin
@@ -267,11 +306,15 @@ module exec_unit #(parameter DATA_BITS = 8) (
                     state       <= DECODE;
                 end
                 DECODE: begin
+                    // Tentatively update the program counter
+                    // to the next instruction
+                    pc_offset_sel <= NEXT_INSTRUCTION;
+
                     // Need to compute the jump target early, otherwise
                     // the instruction right after the jnz is executed
                     // before branching
                     if (ir[15:12] == JNZI) begin
-                        if (~alu_zero) begin
+                        if (~zero_flag) begin
                             pc_offset_sel <= JUMP_TARGET;
                             jump_dest <= rd_ram_data;
                         end
@@ -279,14 +322,25 @@ module exec_unit #(parameter DATA_BITS = 8) (
                     ir[7:0]      <= rd_ram_data;
                     rd_mem_en    <= 0;
                     wr_mem_en    <= 0;
+
+                    request_register_reads();
+
                     state        <= EXECUTE;
                 end
                 EXECUTE: begin
+                    // Program counter updates happened in the previous stage
+                    // No updates until next instruction
+                    pc_offset_sel <= NO_UPDATE;
+
                     execute_instruction();
+
+                    save_alu_flags <= (ir[15:12] == ADDI) | (ir[15:12] == ADDRR) |
+                                      (ir[15:12] == SUBI) | (ir[15:12] == SUBRR);
+
                     if (ir[15:12] == LOAD)
                         state <= LOAD_STAGE;
                     else if (ir[15:12] == STORE)
-                        state <= STORE_START;
+                        state <= STORE_STAGE;
                     else
                         state <= FETCH_MSB_IR;
                 end
@@ -296,14 +350,7 @@ module exec_unit #(parameter DATA_BITS = 8) (
                     rd_mem_en <= 0;
                     state     <= FETCH_MSB_IR;
                 end
-                STORE_START: begin
-                    wr_mem_data           <= regfile_rd0_data;
-                    mem_write_in_progress <= 1;
-                    wr_mem_en             <= 1;
-                    rd_mem_en             <= 0;
-                    state                 <= STORE_END;
-                end
-                STORE_END: begin
+                STORE_STAGE: begin
                     wr_mem_en             <= 0;
                     mem_write_in_progress <= 0;
                     state                 <= FETCH_MSB_IR;
