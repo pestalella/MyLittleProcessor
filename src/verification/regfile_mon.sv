b5038f8669558a1a4ddb0085a07691f5495e72cd
Pau Estalella <pau.estalella@gmail.com>
2020-02-09 21:08:50 +0100

    Fix log formatting

--- src/verification/regfile_mon.sv
+++ src/verification/regfile_mon.sv
@@ -17,7 +17,7 @@ class regfile_mon;
         regfile_trans trans;
         forever begin
             @(posedge vif.reset) begin
-                $display("[%0dns] RF_MONITOR: Reset triggered", $time);
+                $display("[%6dns] RF_MONITOR: Reset triggered", $time);
                 trans = new();
                 trans.action = regfile_trans::RESET;
                 mon2scb.put(trans);
@@ -29,7 +29,7 @@ class regfile_mon;
         regfile_trans trans;
         forever begin
             @(posedge vif.wr_enable) begin
-                $display("[%0dns] RF_MONITOR: Write to register r%0d, value %02h", $time, vif.wr_addr, vif.wr_data);
+                $display("[%6dns] RF_MONITOR: Write to register r%0d, value %02h", $time, vif.wr_addr, vif.wr_data);
                 trans = new();
                 trans.action = regfile_trans::WRITE;
                 trans.dest_reg = vif.wr_addr;
